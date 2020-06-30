fcdffffb4779f0aff33920847725e446a877722f
Dmitrii <dm.nguyen.spb@gmail.com>
2020-06-30 13:15:19 +0300

    Updates

---
+++ precache-manifest.0e6519a6641d2e6fd11d61911bbd74e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20426aa091345a9df7b2483197f5a44c",
+    "url": "/react-todo-ts/index.html"
+  },
+  {
+    "revision": "989e3fd4021c81de105a",
+    "url": "/react-todo-ts/static/css/main.874e2789.chunk.css"
+  },
+  {
+    "revision": "9a27ea13c0965ac76858",
+    "url": "/react-todo-ts/static/js/2.deab529d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-todo-ts/static/js/2.deab529d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "989e3fd4021c81de105a",
+    "url": "/react-todo-ts/static/js/main.d89dc070.chunk.js"
+  },
+  {
+    "revision": "bbcefa9336c0dc0cfe50",
+    "url": "/react-todo-ts/static/js/runtime-main.b4564992.js"
+  }
+]);
